84b2f63834bcda08720aa6117ff482e06b558d5e
Arnav0607 <80532359+Arnav0607@users.noreply.github.com>
2022-03-14 19:47:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,8 +7,8 @@ const Body = Matter.Body;
 
 function preload()
 {
-	helicopterIMG=loadImage("images/helicopter.png")
-	packageIMG=loadImage("images/package.png")
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
 }
 
 function setup() {
